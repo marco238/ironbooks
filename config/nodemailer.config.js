b2ddde5e56a8633cbfdecaad963f7e2377ba2b21
Marcos Monzón <marcos.monzon.iparraguirre.ext@ing.com>
2024-01-11 16:46:42 +0100

    fix mail link

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -16,7 +16,7 @@ module.exports.createEmailTemplate = (user) => {
       <h1>Validate your email 🚀</h1>
       <p>Hi ${user.username} 👋🏻, thanks for registering!</p>
       <p>Click on the following link to validate your email</p>
-      <a href="http://${process.env.HOST}/activate/${user.activationToken}" style="background-color: green;color: whitesmoke;padding: 8px 12px;border-radius: 4px;text-decoration: none;">
+      <a href="${process.env.HOST}/activate/${user.activationToken}" style="background-color: green;color: whitesmoke;padding: 8px 12px;border-radius: 4px;text-decoration: none;">
         Activate
       </a>
     </div>
